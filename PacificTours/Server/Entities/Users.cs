573e7bd9da51298987a778beac0dfe503d19330d
amoyse <securemoyse127@protonmail.com>
2024-01-09 18:46:38 +0000

    first migration

---
+++ PacificTours/Server/Entities/Users.cs
@@ -0,0 +1,13 @@
+namespace PacificTours.Server.Entities;
+
+public class Users
+{
+   public int UserID { get; set; } 
+   public string Name { get; set; } 
+   public string Username { get; set; } 
+   public string Password { get; set; } 
+   public int PhoneNumber { get; set; } 
+   public int PassportNumber { get; set; } 
+   
+   
+}
