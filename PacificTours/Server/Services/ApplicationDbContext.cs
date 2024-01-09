95b2eadba77a73f261abe6d8b2c270dcad740afe
amoyse <securemoyse127@protonmail.com>
2024-01-09 22:27:01 +0000

    redone first migration

--- PacificTours/Server/Services/ApplicationDbContext.cs
+++ PacificTours/Server/Services/ApplicationDbContext.cs
@@ -1,7 +1,8 @@
 namespace PacificTours.Server.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-public class ApplicationDbContext: DbContext
+public class ApplicationDbContext: IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
