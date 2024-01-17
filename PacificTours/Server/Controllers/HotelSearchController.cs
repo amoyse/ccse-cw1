30083df5a8097fe27aa59a2198b8f82b471fbf57
amoyse <securemoyse127@protonmail.com>
2024-01-17 20:48:12 +0000

    sql query converted to ef equivalent

--- PacificTours/Server/Controllers/HotelSearchController.cs
+++ PacificTours/Server/Controllers/HotelSearchController.cs
@@ -19,7 +19,35 @@ public class HotelSearchController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<List<Hotel>>> GetAvailableHotels()
     {
-        var list = await _context.Hotels.ToListAsync();
+        // var list = _context.Database.SqlQuery<string>(
+        //     $"SELECT H.Id, H.Name, CASE WHEN SR.Count < 20 THEN H.SingleCost ELSE NULL END AS SingleCost, CASE WHEN DR.Count < 20 THEN H.DoubleCost ELSE NULL END AS DoubleCost CASE WHEN"
+        //     );
+        
+        DateTime inputStartDate = DateTime.Today;
+        DateTime inputEndDate = DateTime.MaxValue;
+
+        var list = await _context.Hotels.Select(hotel => new
+            {
+                HotelId = hotel.Id,
+                HotelName = hotel.Name,
+                
+                // checks if number of single rooms booked between two dates given is less than 20
+                // if it is, return single room cost, if not return null for single room value
+                SingleRoomCost = _context.HotelBookings
+                    .Count(booking => booking.HotelId == hotel.Id && booking.RoomType == "Single" && 
+                                      booking.StartDate <= inputEndDate && booking.EndDate >= inputStartDate) < 20 
+                    ? hotel.SingleCost : (decimal?) null,
+                DoubleRoomCost = _context.HotelBookings
+                    .Count(booking => booking.HotelId == hotel.Id && booking.RoomType == "Double" && 
+                                      booking.StartDate <= inputEndDate && booking.EndDate >= inputStartDate) < 20 
+                    ? hotel.DoubleCost : (decimal?) null,
+                FamilyRoomCost = _context.HotelBookings
+                    .Count(booking => booking.HotelId == hotel.Id && booking.RoomType == "Family" && 
+                                      booking.StartDate <= inputEndDate && booking.EndDate >= inputStartDate) < 20 
+                    ? hotel.FamilyCost : (decimal?) null
+            })
+            .ToListAsync();
+        
         return Ok(list);
     }
 }
