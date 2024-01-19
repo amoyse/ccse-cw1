d48322848a41038dd8024ddf0a97d44b87b2c43e
amoyse <securemoyse127@protonmail.com>
2024-01-19 22:01:32 +0000

    created bookinginfo data transfer object

--- PacificTours/Server/Controllers/BookingController.cs
+++ PacificTours/Server/Controllers/BookingController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PacificTours.Server.Services;
 using PacificTours.Shared.Entities;
+using PacificTours.Shared;
 
 namespace PacificTours.Server.Controllers;
 
@@ -15,6 +16,24 @@ public class BookingController : ControllerBase
     public BookingController(ApplicationDbContext context)
     {
         _context = context;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Booking>> AddBooking(BookingInfoDto bookingInfo)
+    {
+        
+        // Check if booking a tour or hotel
+        if (bookingInfo.StartDate == bookingInfo.EndDate)
+        {
+            var tour = _context.Tours.Find(bookingInfo.Id);
+            
+
+        }
+        else
+        {
+            
+        }
+        return Ok();
     }
     
 }
