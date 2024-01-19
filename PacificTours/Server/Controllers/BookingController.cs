dd716f6d31f11c124ef96f81577051ca43437c13
amoyse <securemoyse127@protonmail.com>
2024-01-19 19:46:41 +0000

    started work on booking, and amended hotel search date picker

---
+++ PacificTours/Server/Controllers/BookingController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PacificTours.Server.Services;
+using PacificTours.Shared.Entities;
+
+namespace PacificTours.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookingController : ControllerBase
+{
+    
+    private readonly ApplicationDbContext _context;
+
+    public BookingController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+}
